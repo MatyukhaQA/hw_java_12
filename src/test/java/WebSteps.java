a161e6af0af12b5e54de8489898ad5f0511b770c
masha <m@matyuha.ru>
2023-12-01 23:01:36 +0300

    added tests with allure report

---
+++ src/test/java/WebSteps.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchForRepository(String repo) {
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").pressEnter();
+    }
+
+    @Step("Кликаем по ссылке репозитория {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText("eroshenkoam/allure-example")).click();
+    }
+
+    @Step("Открываем таб Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issue с номером {issue}")
+    public void shouldSeeIssueWithNumber(String issue) {
+        $(withText("69 nice")).should(Condition.exist);
+    }
+}
